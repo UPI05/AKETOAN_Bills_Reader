2c113f07f8cc8c257d0c3765a0b8b74d07c73025
Hieu V <votruongtrunghieu@gmail.com>
2022-03-15 16:56:45 +0700

    Add read header

--- main.js
+++ main.js
@@ -20,10 +20,73 @@ const pdfExtract = new PDFExtract();
 
 const options = {};
 
-pdfExtract.extract("./pdfs/8.pdf", options, (err, data) => {
+pdfExtract.extract("./pdfs/10.pdf", options, (err, data) => {
+  const getDataBeginWith = (str, pattern) => {
+    let pos = str.search(pattern);
+    if (pos === -1) return "";
+    let retStr = "";
+    for (let i = pos; i < str.length; i++) retStr += str[i];
+    return retStr;
+  };
+  const getDataAfterColon = (str) => {
+    let pos = -1;
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === ":") {
+        pos = i;
+        break;
+      }
+    }
+    if (pos === -1) return "";
+    let retStr = "";
+    for (let i = pos + 1; i < str.length; i++) retStr += str[i];
+    return retStr;
+  };
+  const removeVietnameseTones = (str) => {
+    str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+    str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+    str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+    str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+    str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+    str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+    str = str.replace(/đ/g, "d");
+    str = str.replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g, "A");
+    str = str.replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g, "E");
+    str = str.replace(/Ì|Í|Ị|Ỉ|Ĩ/g, "I");
+    str = str.replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g, "O");
+    str = str.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, "U");
+    str = str.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, "Y");
+    str = str.replace(/Đ/g, "D");
+    // Some system encode vietnamese combining accent as individual utf-8 characters
+    // Một vài bộ encode coi các dấu mũ, dấu chữ như một kí tự riêng biệt nên thêm hai dòng này
+    str = str.replace(/\u0300|\u0301|\u0303|\u0309|\u0323/g, ""); // ̀ ́ ̃ ̉ ̣  huyền, sắc, ngã, hỏi, nặng
+    str = str.replace(/\u02C6|\u0306|\u031B/g, ""); // ˆ ̆ ̛  Â, Ê, Ă, Ơ, Ư
+    // Remove extra spaces
+    // Bỏ các khoảng trắng liền nhau
+    str = str.replace(/ + /g, " ");
+    str = str.trim();
+    // Remove punctuations
+    // Bỏ dấu câu, kí tự đặc biệt
+    // str = str.replace(/!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'|\"|\&|\#|\[|\]|~|\$|_|`|-|{|}|\||\\/g," ");
+    return str;
+  };
+  const standardStr = (str) => {
+    str = removeVietnameseTones(str);
+    let retStr = "";
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === " ") continue;
+      asciiCode = str[i].charCodeAt(0);
+      if (asciiCode >= 65 && asciiCode <= 90)
+        retStr += String.fromCharCode(asciiCode + 32);
+      // if (asciiCode >= 97 && asciiCode <= 122) retStr += str[i];
+      else retStr += str[i];
+    }
+    return retStr;
+  };
+
   if (err) return console.log(err);
 
   let randomData = data.pages[0].content;
+  // for (let i = 0; i < randomData.length; i++) randomData[i].y = Math.floor(randomData[i].y);
 
   /*
   // Multi pages
@@ -59,10 +122,8 @@ pdfExtract.extract("./pdfs/8.pdf", options, (err, data) => {
     }
   }
 
-  fs.writeFileSync('data.json', JSON.stringify(sortedData));
-
   // Print dataMatrix
-/*
+  /*
   let cnt = 0;
   let found = 0;
   for (let i = 0; i < dataMatrix.length; i++) {
@@ -82,7 +143,6 @@ pdfExtract.extract("./pdfs/8.pdf", options, (err, data) => {
   }
   console.log(cnt);*/
 
-
   // Extract data into header, body and footer
 
   let header = new Array();
@@ -92,8 +152,16 @@ pdfExtract.extract("./pdfs/8.pdf", options, (err, data) => {
   let sttFound = false;
 
   let posI, posJ;
-
+  let posH = 0;
   for (let i = 0; i < dataMatrix.length; i++) {
+    let str = "";
+    for (let j = 0; j < dataMatrix[i].length; j++) str += dataMatrix[i][j].str;
+    if (str.search("Mẫu số") != -1) {
+      posH = i;
+      break;
+    }
+  }
+  for (let i = posH; i < dataMatrix.length; i++) {
     if (sttFound) break;
     for (let j = 0; j < dataMatrix[i].length; j++) {
       posI = i;
@@ -109,12 +177,71 @@ pdfExtract.extract("./pdfs/8.pdf", options, (err, data) => {
 
   console.log("\nHeader: ");
 
+  let taxId1Found = 0,
+    address1Found = 0,
+    companyFound = 0;
+
   for (let i = 0; i < header.length; i++) {
     let str = "";
     for (let j = 0; j < header[i].length; j++) {
       str += header[i][j].str;
     }
-    console.log(str);
+    standardedStr = standardStr(str);
+
+    if (standardedStr.match("mauso")) {
+      console.log(`Mau so: ${getDataAfterColon(str)}`);
+    }
+    if (standardedStr.match("kyhieu")) {
+      console.log(`Ky hieu: ${getDataAfterColon(str)}`);
+    }
+    if (standardedStr.match("so:") || standardedStr.match(/so\(/)) {
+      console.log(`So: ${getDataAfterColon(str)}`);
+    }
+    if (standardedStr.match("hinhthucthanhtoan")) {
+      console.log(`Hinh thuc thanh toan: ${getDataAfterColon(str)}`);
+    }
+    if (standardedStr.match("diachi:") || standardedStr.match(/diachi\(/)) {
+      if (address1Found) {
+        console.log(`Dia chi don vi mua: ${getDataAfterColon(str)}`);
+      } else {
+        console.log(`Dia chi don vi ban: ${getDataAfterColon(str)}`);
+        address1Found = true;
+      }
+    }
+    if (standardedStr.match("masothue:") || standardedStr.match(/masothue\(/)) {
+      if (taxId1Found) {
+        console.log(`Ma so thue don vi mua: ${getDataAfterColon(str)}`);
+      } else {
+        console.log(`Ma so thue don vi ban: ${getDataAfterColon(str)}`);
+        taxId1Found = true;
+      }
+    }
+    if (standardedStr.match("ngay")) {
+      console.log(`Ngay: ${getDataBeginWith(str, "Ngày")}`);
+    }
+    if (standardedStr.match("nguoimua")) {
+      console.log(`Nguoi mua: ${getDataAfterColon(str)}`);
+    }
+    if (
+      standardedStr.match("congty") ||
+      standardedStr.match("congti") ||
+      standardedStr.match("dntn")
+    ) {
+      // remember to check if stk includes chinhanh || standardedStr.match("chinhanh")) {
+      let dt;
+      if (str.search(":") !== -1) dt = getDataAfterColon(str);
+      else {
+        dt = getDataBeginWith(str, "CÔNG TY");
+        if (!dt) dt = getDataBeginWith(str, "Công Ty");
+        if (!dt) dt = getDataBeginWith(str, "Công ty");
+      }
+      if (companyFound) {
+        console.log(`Don vi mua: ${dt}`);
+      } else {
+        console.log(`Don vi ban: ${dt}`);
+        companyFound = true;
+      }
+    }
   }
 
   sttFound = false;
@@ -125,7 +252,10 @@ pdfExtract.extract("./pdfs/8.pdf", options, (err, data) => {
     for (let j = 0; j < dataMatrix[i].length; j++) {
       posIF = i;
       posJF = j;
-      if (dataMatrix[i][j].str.search("Cộng tiền hàng") !== -1 || dataMatrix[i][j].str.search("Tiền hàng hóa") !== -1) {
+      if (
+        dataMatrix[i][j].str.search("Cộng tiền hàng") !== -1 ||
+        dataMatrix[i][j].str.search("Tiền hàng hóa") !== -1
+      ) {
         sttFound = true;
         break;
       }
@@ -133,14 +263,14 @@ pdfExtract.extract("./pdfs/8.pdf", options, (err, data) => {
       body[body.length - 1].push(dataMatrix[i][j]);
     }
   }
-  console.log("\nBody: ");
+  // console.log("\nBody: ");
 
   for (let i = 0; i < body.length; i++) {
     let str = "";
     for (let j = 0; j < body[i].length; j++) {
       str += body[i][j].str;
     }
-    console.log(str);
+    // console.log(str);
   }
 
   sttFound = false;
@@ -153,8 +283,9 @@ pdfExtract.extract("./pdfs/8.pdf", options, (err, data) => {
     }
   }
 
-  console.log("\nFooter: ");
+  // console.log("\nFooter: ");
 
+  /*
   for (let i = 0; i < footer.length; i++) {
     let str = "";
     for (let j = 0; j < footer[i].length; j++) {
@@ -162,4 +293,7 @@ pdfExtract.extract("./pdfs/8.pdf", options, (err, data) => {
     }
     console.log(str);
   }
+  */
+
+  fs.writeFileSync("data.json", JSON.stringify(dataMatrix));
 });
