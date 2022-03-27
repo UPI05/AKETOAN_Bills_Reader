b18017560ae062e6a30551c05686569a0ed31f2a
Hieu V <votruongtrunghieu@gmail.com>
2022-03-27 11:31:04 +0700

    Edit mailreader

--- mailreader.js
+++ mailreader.js
@@ -1,61 +1,47 @@
 const { ImapFlow } = require("imapflow");
-const dotenv = require("dotenv");
-const fs = require('fs');
+const fs = require("fs");
 
-dotenv.config({ path: ".env.local" });
+const readMailAndUploadFile = async (host, port, auth) => {
+  const client = new ImapFlow({
+    host,
+    port,
+    secure: true,
+    auth,
+  });
 
-const client = new ImapFlow({
-  host: "imap.gmail.com",
-  port: 993,
-  secure: true,
-  auth: {
-    user: "aketoan12345@gmail.com",
-    pass: process.env.passwd,
-  },
-});
-
-const writePdfFromBase64 = (code, path) => {
-  fs.writeFile(
-    path,
-    code,
-    { encoding: "base64" },
-    async (err, data) => {
+  const writePdfFromBase64 = (code, path) => {
+    fs.writeFile(path, code, { encoding: "base64" }, async (err, data) => {
       if (err) {
         console.log("err", err);
       }
       console.log("success");
-    }
-  );
-}
+    });
+  };
 
-const main = async () => {
   // Wait until client connects and authorizes
   await client.connect();
 
   // Select and lock a mailbox. Throws if mailbox does not exist
   let lock = await client.getMailboxLock("INBOX");
   try {
-    for await (let message of client.fetch(
-      { seen: false },
-      { source: true }
-    )) {
+    for await (let message of client.fetch({ seen: false }, { source: true })) {
       const str = message.source.toString();
-      const pos = str.search('X-Attachment-Id');
+      const pos = str.search("X-Attachment-Id");
 
       if (pos === -1) continue;
 
       let cnt = 0;
       for (let i = pos; i < str.length; i++) {
-        if (str[i] == '\n') cnt++;
+        if (str[i] == "\n") cnt++;
         if (cnt === 2) {
-          let s = '';
+          let s = "";
           for (let j = i; j < str.length; j++) {
-            if (str[j] !== ' ') s += str[j];
+            if (str[j] !== " ") s += str[j];
             else break;
           }
-          let tail = '';
-          if (str.search('Content-Type: application/pdf') !== -1) tail = '.pdf';
-          else if (str.search('Content-Type: text/html') !== -1) tail = '.xml';
+          let tail = "";
+          if (str.search("Content-Type: application/pdf") !== -1) tail = ".pdf";
+          else if (str.search("Content-Type: text/html") !== -1) tail = ".xml";
           else continue;
           writePdfFromBase64(s, `./downloads/${Date.now()}${tail}`);
           break;
@@ -71,7 +57,10 @@ const main = async () => {
   await client.logout();
 };
 
-main()
+readMailAndUploadFile("imap.gmail.com", 993, {
+  user: "aketoan12345@gmail.com",
+  pass: "",
+})
   .then((res) => {
     console.log("done");
   })
